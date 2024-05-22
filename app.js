43821481f182850f65eca7aedb5f4d076cd2b651
AaronG24 <arongonzalez2411@gmail.com>
2024-05-22 16:09:33 -0400

    operadores y condicionales

---
+++ app.js
@@ -0,0 +1,33 @@
+let suma = 2 + 5
+
+let resta = 3 - 3
+
+//Esta varioble es una multiplicacion
+let multiplicacion = 50 * 5
+
+//Operadores logicos: SI (&&) y NO (||)
+//Operadores logicos : < (Menor que), > (Mayor que), <= (Menor o igual), >= (Mayor o igual)
+
+//esta variable es menorQue 
+let menorQue = 2 < 1
+
+//esta variable es mayorQue
+let mayorQue = 2 > 1
+
+//esta variable es menorOIgualQue
+let menorOIgualQue = 2 <= 3;
+
+//esta variable es mayorOIgualQue
+let mayorOIgualQue = 1 >= 5
+
+
+let y = ((2 > 1) && (4 > 1));
+
+let n = 5 > Number.MAX_SAFE_INTEGER || 2 < Number.MAX_SAFE_INTEGER
+
+if (2 == 4) {
+    console.log('Es igual.')
+} else {
+    console.log('No es igual.')
+}
+
